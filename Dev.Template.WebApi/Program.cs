b3a268d76cbb22b96505d7fd096eed270e3f8fbb
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-09-18 00:16:42 +0530

    ®️ Service Registration Update

--- Dev.Template.WebApi/Program.cs
+++ Dev.Template.WebApi/Program.cs
@@ -1,27 +1,28 @@
-using Dev.Template.Repositories;
-using Dev.Template.Model.Entity;
-using Dev.Template.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Genesis.Repositories;
+using Dev.Template.DBContext;
+using Genesis.Services;
+using Genesis.Controllers.Extension;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure database context
-builder.Services.AddRepositories<AppRepositories>(options =>
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration["ConnectionString:SqlServer"],
                          x =>
                          {
                              x.MigrationsAssembly("Dev.Template.Migrations.SqlServer");
                          }));
 
-// Add Repositories services
-builder.Services.AddScoped(typeof(IRepositoriesBase<>), typeof(RepositoriesBase<>));
-builder.Services.AddScoped<ICountHistoryRepositories, CountHistoryRepositories>();
+builder.Services.AddScoped<IDbContext>(provider =>
+    provider.GetRequiredService<AppDbContext>());
 
-// Add controllers
+builder.Services.AddScopedByConvention(Assembly.GetExecutingAssembly());
+builder.Services.AddGenesisScoped();
+
 builder.Services.AddControllers();
 
-// Configure CORS to allow all origins
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllOrigins",
@@ -33,7 +34,6 @@ builder.Services.AddCors(options =>
         });
 });
 
-// Add Swagger for API documentation
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -50,7 +50,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = "swagger"; // Set Swagger UI to be at /swagger/index.html
+        c.RoutePrefix = "swagger";
     });
 }
 else
@@ -62,13 +62,11 @@ else
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-// Ensure CORS is used before routing
 app.UseCors("AllowAllOrigins");
 
 app.UseRouting();
 app.UseAuthorization();
 
-// Map controllers to endpoints
 app.MapControllers();
 
 app.Run();
